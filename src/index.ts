5003ffd3a35047c8e01dd0a3fe78a9cf7fd541c8
ntt236 <nguyenthanhtrieu236@gmail.com>
2025-11-27 11:16:27 +0700

    fix : cors connect vite

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,10 @@ import authRoutes from "./routes/authRoutes"
 dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 4000;
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
 app.use(express.json());
 
 
